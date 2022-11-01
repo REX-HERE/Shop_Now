1a1fb86059c2a0ed0849703c89208e255ae066e0
REX-HERE <punit.kumarmeena.cse20@gmail.com>
2022-11-01 17:27:10 +0530

    demos

---
+++ BackendShopNow/src/main/java/com/ShopNow/Controller/UserControlled.java
@@ -0,0 +1,19 @@
+package com.ShopNow.Controller;
+
+import com.ShopNow.DAO.UserDAO;
+import com.ShopNow.Models.user;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserControlled {
+    @Autowired
+    UserDAO userDAO;
+    @GetMapping("/all/users")
+    List<user> fetchAll(){
+        return userDAO.getAllusers();
+    }
+}
---
+++ BackendShopNow/src/main/java/com/ShopNow/DAO/UserDAO.java
@@ -0,0 +1,28 @@
+package com.ShopNow.DAO;
+
+import com.ShopNow.Models.user;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class UserDAO {
+    @Autowired
+    JdbcTemplate jdbc;
+
+    public List<user> getAllusers(){
+        try{
+            user x = new user();
+
+            String query = "SELECT * FROM user where first_name=?";
+            List<user> result = jdbc.query(query, new BeanPropertyRowMapper<>(user.class));
+            return result;
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+}
