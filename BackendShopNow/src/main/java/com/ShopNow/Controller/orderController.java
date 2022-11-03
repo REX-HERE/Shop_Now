0e37714caf279774a07ccfe694eebb9c27eb3ae8
REX-HERE <punit.kumarmeena.cse20@gmail.com>
2022-11-03 18:44:04 +0530

    post order api done all parts and tested

--- BackendShopNow/src/main/java/com/ShopNow/Controller/orderController.java
+++ BackendShopNow/src/main/java/com/ShopNow/Controller/orderController.java
@@ -2,21 +2,37 @@ package com.ShopNow.Controller;
 
 import com.ShopNow.DAO.orderDao;
 import com.ShopNow.Models.product;
+import com.ShopNow.Models.shoppingCart;
 import com.ShopNow.Models.user;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class orderController {
     @Autowired
     orderDao orderDao;
-    @PostMapping("/orders")
-    public Integer postOrder(@RequestBody List<product> productData, user userData){
-        return orderDao.insertOrder(productData, userData);
+
+    @PostMapping("/order")
+    public Integer postOrder(@RequestBody List<shoppingCart> cartData){
+        String userId = "";
+        List<String> productIdList = new ArrayList<>();
+        for (int i=0;i<1;i++){
+            userId = cartData.get(i).getUserId();
+        }
+        if(userId==""){
+            System.out.println("cart empty");
+            return 0;
+        }
+
+        cartData.forEach((tuple)->{
+            productIdList.add(tuple.getProductId());
+        });
+        return orderDao.insertOrder(productIdList, userId);
     }
 
 
