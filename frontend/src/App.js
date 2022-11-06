170bd0f74e819c3d918727bb4ef411dcc55f4053
REX-HERE <punit.kumarmeena.cse20@gmail.com>
2022-11-06 20:16:16 +0530

    frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,40 +1,27 @@
 import React, {useState} from "react";
-import './App.css';
-import {Routes, Route} from 'react-router-dom';
 
-import Navbar from "./components/items/navbar/Navbar";
-import { TabPanelList } from "./components/items/navbar/tabPanelList";
-import { LogInfo } from "./constants/constant_vals";
-// import history from "./history";
-import { Provider } from 'react-redux';
-import store from './state/store/store';
+import { LogInfo } from './constants/constant_vals';
+import {Routes, Route, Link} from 'react-router-dom';
+import navBar from "./components/navbar/navbar";
+import home from "./components/home/home";
 
-const App = () => {
-    if(LogInfo){
-      console.info(`rendering App.js`)
-    }
-    const [error_app, seterror_app] = useState(false);
+function App() {
+  if(LogInfo){
+    console.info(`rendering App.js`)
+  }
 
-    const seterror_app_handler = () => {
-      seterror_app(true)
-    }
+  return (
 
+    <Routes>
+      <navBar/>
+      <Route path='/' element={<home/>}></Route>
+      <Route path='/products' element={<home/>}></Route>
+      <Route path='/products/details' element={<home/>}></Route>
+      <Route path='/cart' element={<home/>}></Route>
 
-    return (
-      <Provider store={store}>
+    </Routes>
 
-        <Routes>
-          <Navbar errorHandler={seterror_app_handler}/>
-          <TabPanelList/>
-          
-          <Route path="/" element={<Home/>}></Route>
-
-        </Routes>
-      </Provider>
-    )
+  );
 }
 
 export default App;
-
-
-
